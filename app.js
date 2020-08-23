a2cc0844756b2404732cf46e5464d64824687d64
aatitkathariya <70094804+aatitkathariya@users.noreply.github.com>
2020-08-23 16:25:40 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB_EgJZEjPl1VWx1-44MuFKcr2gRLWWb4U",
+  authDomain: "sdpb-9cebf.firebaseapp.com",
+  databaseURL: "https://sdpb-9cebf.firebaseio.com",
+  projectId: "sdpb-9cebf",
+  storageBucket: "sdpb-9cebf.appspot.com",
+  messagingSenderId: "367237998445",
+  appId: "1:367237998445:web:fcd43b774275dccff89c64",
+  measurementId: "G-LC2ZR1VL70"
 };
-
      
  
 ////Do Not Edit Below This
